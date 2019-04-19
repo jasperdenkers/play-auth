2cff5124b05d53fd6011c5dc206cfb4214bd59bb
Jasper Denkers <jasperdenkers@gmail.com>
2019-04-19 08:32:57 +0200

    Pluggable authentication for Authorized; improve modularity

--- core/app/com/jasperdenkers/play/auth/Authorization.scala
+++ core/app/com/jasperdenkers/play/auth/Authorization.scala
@@ -11,18 +11,22 @@ trait Authorization[A] extends AuthRequests[A] { self: Authentication[A] with Ba
 
   def onUnauthorized[B](authorizedRequest: AuthorizedRequestWithIdentity[B]): Future[Result] = authorizator.notAuthorizedResult(authorizedRequest)
 
-  def Authorized(capabilities: Capability*) = Authenticated andThen new ActionFunction[AuthenticatedRequestWithIdentity, AuthorizedRequestWithIdentity] {
-    def executionContext = self.defaultExecutionContext
-    def invokeBlock[B](authenticatedRequest: AuthenticatedRequestWithIdentity[B], block: (AuthorizedRequestWithIdentity[B]) => Future[Result]) =
-      authorizator.getTokens(authenticatedRequest).flatMap { tokens =>
-        val authorizedRequest = AuthorizedRequest.fromAuthenticatedRequest(authenticatedRequest, tokens)
+  def Authorized(authentication: BaseActionBuilder[AuthenticatedRequestWithIdentity], capabilities: Capability*): ActionBuilder[AuthorizedRequestWithIdentity, AnyContent] =
+    authentication andThen new ActionFunction[AuthenticatedRequestWithIdentity, AuthorizedRequestWithIdentity] {
+      def executionContext = self.defaultExecutionContext
+      def invokeBlock[B](authenticatedRequest: AuthenticatedRequestWithIdentity[B], block: AuthorizedRequestWithIdentity[B] => Future[Result]) =
+        authorizator.getTokens(authenticatedRequest).flatMap { tokens =>
+          val authorizedRequest = AuthorizedRequest.fromAuthenticatedRequest(authenticatedRequest, tokens)
 
-        if (authorizedRequest.isAuthorized(capabilities.toSet))
-          block(authorizedRequest)
-        else
-          onUnauthorized(authorizedRequest)
-      }
-  }
+          if (authorizedRequest.isAuthorized(capabilities.toSet))
+            block(authorizedRequest)
+          else
+            onUnauthorized(authorizedRequest)
+        }
+    }
+
+  def Authorized(capabilities: Capability*): ActionBuilder[AuthorizedRequestWithIdentity, AnyContent] =
+    Authorized(Authenticated, capabilities :_ *)
 
   def MaybeAuthorized = MaybeAuthenticated andThen new BaseActionBuilder[Request] {
     def invokeBlock[B](request: Request[B], block: (Request[B]) => Future[Result]) = {
